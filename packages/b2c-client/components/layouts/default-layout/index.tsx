d0c36b8d8fa848a2083878d82459713477d4d202
Xu <hieudd0899@gmail.com>
2024-05-09 09:46:39 +0700

    Fix: Change DB from Mongodb Atlas to My SQL

--- packages/b2c-client/components/layouts/default-layout/index.tsx
+++ packages/b2c-client/components/layouts/default-layout/index.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { Navbar } from './navbar';
 
 type Props = {
     children: React.ReactNode;
 };
 
 export const DefaultLayout: React.FC<Props> = ({ children }) => {
-    return (
-        <div>
-            <Navbar />
-            {children}
-        </div>
-    );
+    return <div>{children}</div>;
 };
