59e45a258c23986a3656546324d961236e78d21b
Xu <hieudd0899@gmail.com>
2024-05-17 09:28:21 +0700

    BE: Done api upload images

--- packages/server/src/app/routers/index.ts
+++ packages/server/src/app/routers/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import auth from './auth';
 import user from './user';
 import brand from './brand';
+import upload from './upload';
 
 const router = Router();
 
@@ -9,6 +10,7 @@ export default (): Router => {
     auth(router);
     user(router);
     brand(router);
+    upload(router);
 
     return router;
 };
