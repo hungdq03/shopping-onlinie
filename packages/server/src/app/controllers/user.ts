f2ec495cd76271548c82bf57a58fbb40ace2e525
hungdao <hung2k3vn@gmail.com>
2024-05-21 00:51:05 +0700

    BE: create user

--- packages/server/src/app/controllers/user.ts
+++ packages/server/src/app/controllers/user.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import { jwtDecode } from 'jwt-decode';
-import { SuccessResponseType } from '../../constant';
+import { hashSync } from 'bcrypt';
+import {
+    ERROR_RES,
+    PAGE_SIZE,
+    SALT,
+    SuccessResponseType,
+} from '../../constant';
 import { db } from '../../lib/db';
 import { TokenDecoded } from '../../types';
 import { getToken } from '../../lib/utils';
@@ -39,27 +45,216 @@ export const getUser = async (req: Request, res: Response) => {
 };
 
 export const getListUser = async (req: Request, res: Response) => {
+    const {
+        search,
+        searchBy,
+        pageSize,
+        currentPage,
+        sortBy,
+        fillterBy,
+        fillter,
+    } = req.query;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const where: any = {};
+
+    if (searchBy && search) {
+        where[String(searchBy)] = { contains: String(search) };
+    }
+
+    if (fillterBy && fillter) {
+        where[String(fillterBy)] = String(fillter);
+    }
+
+    const prismaQuery = {
+        skip: (Number(currentPage) - 1) * Number(pageSize || PAGE_SIZE),
+        take: Number(pageSize),
+        where,
+    };
+
     try {
-        const listUser = await db.user.findMany({
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                gender: true,
-                address: true,
-                phone: true,
-                image: true,
-                role: true,
-                status: true,
+        const total = await db.user.count({
+            where: {
+                name: {
+                    contains: String(search || ''),
+                },
             },
         });
 
-        return res.status(201).json({
+        let listUser;
+
+        switch (sortBy) {
+            case 'LATEST':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        createdAt: 'desc',
+                    },
+                });
+                break;
+            case 'OLDEST':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        createdAt: 'asc',
+                    },
+                });
+                break;
+            case 'NAME_A_TO_Z':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        name: 'asc',
+                    },
+                });
+                break;
+            case 'NAME_Z_TO_A':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        name: 'desc',
+                    },
+                });
+                break;
+            case 'EMAIL_A_TO_Z':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        email: 'asc',
+                    },
+                });
+                break;
+            case 'EMAIL_Z_TO_A':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        email: 'desc',
+                    },
+                });
+                break;
+            case 'GENDER_A_TO_Z':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        gender: 'asc',
+                    },
+                });
+                break;
+            case 'GENDER_Z_TO_A':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        gender: 'desc',
+                    },
+                });
+                break;
+            case 'PHONE_LOW_TO_HIGH':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        phone: 'asc',
+                    },
+                });
+                break;
+            case 'PHONE_HIGH_TO_LOW':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        phone: 'desc',
+                    },
+                });
+                break;
+            case 'ROLE_A_TO_Z':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        role: 'asc',
+                    },
+                });
+                break;
+            case 'ROLE_Z_TO_A':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        role: 'desc',
+                    },
+                });
+                break;
+            case 'STATUS_A_TO_Z':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        status: 'asc',
+                    },
+                });
+                break;
+            case 'STATUS_Z_TO_A':
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        status: 'desc',
+                    },
+                });
+                break;
+            default:
+                listUser = await db.user.findMany({
+                    ...prismaQuery,
+                    orderBy: {
+                        createdAt: 'desc',
+                    },
+                });
+        }
+
+        return res.status(200).json({
             isOk: true,
             data: listUser,
             message: 'Get list user successfully!',
+            pagination: {
+                total,
+            },
         });
     } catch (error) {
         return res.send(500);
     }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+    const { password, name, email, role, gender, address, dob, phone, image } =
+        req.body;
+
+    let user = await db.user.findFirst({
+        where: { email },
+    });
+
+    if (user) {
+        return res.status(400).json({
+            ...ERROR_RES,
+            errors: { message: 'Account already exist!' },
+        });
+    }
+
+    user = await db.user.create({
+        data: {
+            name,
+            email,
+            hashedPassword: hashSync(password, SALT),
+            phone,
+            role,
+            gender,
+            address,
+            image,
+            dob,
+            status: 'NEWLY_REGISTER',
+        },
+    });
+
+    const successObj: SuccessResponseType = {
+        data: {
+            data: user,
+            meta_data: undefined,
+        },
+        message: 'Create new user successfully!',
+    };
+
+    return res.status(200).json(successObj);
 };
