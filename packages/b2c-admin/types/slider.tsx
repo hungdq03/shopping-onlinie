11a9ec069c56a2fe76ced0de6364213f55305563
hoangpvhe170186 <hoangpvhe170186@gmail.com>
2024-05-22 21:28:58 +0700

    FE:CRUD slider

--- packages/b2c-admin/types/slider.tsx
+++ packages/b2c-admin/types/slider.tsx
@@ -1,7 +1,6 @@
 export type Slider = {
-    status: 'active' | 'inactive'; // Updated status field
     id?: string | null;
-    name?: string | null;
+    title?: string | null;
     image?: string | null;
     description?: string | null;
     createdAt?: string | null;
@@ -9,4 +8,38 @@ export type Slider = {
     isshow?: boolean | null;
     createAt?: string | null;
     updateAt?: string | null;
+    product?: Product | null;
+};
+
+export type Brand = {
+    id?: string | null | null;
+    name?: string | null;
+    createdAt?: string | null;
+    updatedAt?: string | null;
+};
+
+export type Category = {
+    id?: string | null;
+    name?: string | null;
+    createdAt?: string | null;
+    updatedAt?: string | null;
+};
+
+export type Product = {
+    id?: string | null;
+    name?: string | null;
+    brandId?: string | null;
+    original_price?: number | null;
+    discount_price?: number | null;
+    quantity?: number | null;
+    sold_quantity?: number | null;
+    description?: string | null;
+    size?: number | null;
+    category?: Category | null;
+    thumbnail?: string | null;
+    rating?: number | null;
+    isShow?: boolean | null;
+    createdAt?: string | null;
+    updatedAt?: string | null;
+    brand?: Brand | null;
 };
