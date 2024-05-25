f7b6b0e410fcfcfd0f771e8108b7845c6e647253
Xu <hieudd0899@gmail.com>
2024-05-25 11:58:58 +0700

    FE: Fix sidebar for marketer

--- packages/b2c-admin/components/layouts/marketer-layout/marketer-sidebar.tsx
+++ packages/b2c-admin/components/layouts/marketer-layout/marketer-sidebar.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -48,10 +49,49 @@ const items: MenuItem[] = [
 ];
 
 const MarketerSidebar = () => {
+    const router = useRouter();
+
+    const activeKey = useMemo(() => {
+        if (router.pathname.startsWith('/marketer/product/brand')) {
+            return ['brand'];
+        }
+        if (router.pathname.startsWith('/marketer/product/category')) {
+            return ['category'];
+        }
+        if (router.pathname.startsWith('/marketer/product')) {
+            return ['productList'];
+        }
+        if (router.pathname.startsWith('/marketer/post')) {
+            return ['post'];
+        }
+        if (router.pathname.startsWith('/marketer/slider')) {
+            return ['slider'];
+        }
+        if (router.pathname.startsWith('/marketer/customer')) {
+            return ['customer'];
+        }
+        if (router.pathname.startsWith('/marketer/feedback')) {
+            return ['feedback'];
+        }
+        if (router.pathname.startsWith('/marketer/dashboard')) {
+            return ['dashboard'];
+        }
+        return undefined;
+    }, [router.pathname]);
+
+    const openKey = useMemo(() => {
+        if (router.pathname.startsWith('/marketer/product')) {
+            return ['product'];
+        }
+        return undefined;
+    }, [router.pathname]);
+
     return (
         <Menu
+            defaultOpenKeys={openKey}
             items={items}
             mode="inline"
+            selectedKeys={activeKey}
             style={{ width: 256, height: 'calc(100vh - 76px)' }}
         />
     );
