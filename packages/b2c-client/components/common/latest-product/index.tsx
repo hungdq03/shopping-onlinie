2212788fd8184b58251f0931afc08329e3a03515
Xu <hieudd0899@gmail.com>
2024-06-19 18:01:49 +0700

    FE: Create UI product detail

--- packages/b2c-client/components/common/latest-product/index.tsx
+++ packages/b2c-client/components/common/latest-product/index.tsx
@@ -11,11 +11,11 @@ const LatestProductList = () => {
         queryFn: () => request.get('/product/latest').then((res) => res.data),
     });
     return (
-        <div className="customScroll space-y- h-full w-full">
+        <div className="customScroll h-full w-full space-y-5 px-5">
             <div className="sticky top-0 z-20 bg-white py-2 text-xl font-bold">
                 Sản phẩm mới
             </div>
-            <div className=" flex flex-col gap-5 px-10 py-2">
+            <div className="flex flex-col gap-5 px-1 py-2">
                 {data?.data?.map((item) => (
                     <LatestProductItem data={item} key={item?.id} />
                 ))}
