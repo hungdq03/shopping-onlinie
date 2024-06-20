6b714d399ba36282c2212868ae5ae0cf8bcddf72
Xu <hieudd0899@gmail.com>
2024-06-20 22:14:32 +0700

    FE: Create form create comment

--- packages/b2c-client/pages/product/[id].tsx
+++ packages/b2c-client/pages/product/[id].tsx
@@ -4,7 +4,8 @@ import * as request from 'common/utils/http-request';
 import { useRouter } from 'next/router';
 import { QueryResponseGetOneType } from 'common/types';
 import { Product } from 'common/types/product';
-import { Spin } from 'antd';
+import { Breadcrumb, Spin } from 'antd';
+import Link from 'next/link';
 import LatestProductList from '~/components/common/latest-product';
 import ProductDetail from '~/components/product/product-details';
 import { NextPageWithLayout } from '../_app';
@@ -21,14 +22,59 @@ const ProductDetailPage: NextPageWithLayout = () => {
         enabled: !!query?.id,
     });
     return (
-        <div className="mt-20 flex px-10">
-            <div className="sticky top-10 hidden h-[90vh] w-[350px] min-w-[350px] xl:block">
-                <LatestProductList />
-            </div>
-            <div className="flex-1">
-                <Spin spinning={isFetching}>
-                    <ProductDetail data={data?.data} />
-                </Spin>
+        <div>
+            <div className="mt-20 flex px-10">
+                <div className="sticky top-10 hidden h-[90vh] w-[350px] min-w-[350px] xl:block">
+                    <LatestProductList />
+                </div>
+                <div className="flex-1">
+                    <div className="container mb-5">
+                        <Breadcrumb
+                            items={[
+                                {
+                                    title: <Link href="/">Trang chủ</Link>,
+                                },
+                                {
+                                    title: (
+                                        <Link
+                                            href={{
+                                                pathname: '/product',
+                                                query: {
+                                                    category:
+                                                        data?.data?.category
+                                                            ?.id,
+                                                },
+                                            }}
+                                        >
+                                            {data?.data?.category?.name}
+                                        </Link>
+                                    ),
+                                },
+                                {
+                                    title: (
+                                        <Link
+                                            href={{
+                                                pathname: '/product',
+                                                query: {
+                                                    brand: data?.data?.brand
+                                                        ?.id,
+                                                },
+                                            }}
+                                        >
+                                            {data?.data?.brand?.name}
+                                        </Link>
+                                    ),
+                                },
+                                {
+                                    title: data?.data?.name,
+                                },
+                            ]}
+                        />
+                    </div>
+                    <Spin spinning={isFetching}>
+                        <ProductDetail data={data?.data} />
+                    </Spin>
+                </div>
             </div>
         </div>
     );
